b64c01cec74dd59f3da303b7b1de4eb56cb13dc3
Тимур <timur-khvan@mail.ru>
2025-05-24 00:40:02 +0300

    feat: [TBPT2-102] добавлен эндпоинт для получения списка активных участников поездки

--- src/main/java/ru/tbank/itis/tripbackend/service/MemberService.java
+++ src/main/java/ru/tbank/itis/tripbackend/service/MemberService.java
@@ -1,6 +1,7 @@
 package ru.tbank.itis.tripbackend.service;
 
 import ru.tbank.itis.tripbackend.dto.TripInvitationDto;
+import ru.tbank.itis.tripbackend.dto.TripParticipantDto;
 import ru.tbank.itis.tripbackend.dto.common.SimpleResponse;
 import ru.tbank.itis.tripbackend.dto.request.InviteRequest;
 import ru.tbank.itis.tripbackend.model.User;
@@ -10,4 +11,5 @@ import java.util.List;
 public interface MemberService {
     SimpleResponse inviteMember(Long tripId, User creator, InviteRequest inviteRequest);
     SimpleResponse removeMember(Long tripId, Long userId, User creator);
+    List<TripParticipantDto> getActiveMembers(Long tripId, User currentUser);
 }
