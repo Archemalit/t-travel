6b8cf210679c99be847b6bb79b9c21bffb8711eb
Archemalit <ArsenDivirov@yandex.ru>
2025-05-24 14:58:59 +0300

    feat: [TBPT2-106] добавил Swagger документацию для TripController

--- src/main/java/ru/tbank/itis/tripbackend/dto/request/TripRequest.java
+++ src/main/java/ru/tbank/itis/tripbackend/dto/request/TripRequest.java
@@ -1,5 +1,6 @@
 package ru.tbank.itis.tripbackend.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
@@ -10,23 +11,29 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Данные о поездке")
 public class TripRequest {
     @NotBlank(message = "Название обязательно")
     @Size(max = 100, message = "Название должно быть менее 100 символов")
+    @Schema(description = "Название поездки", example = "Поездка в Москву")
     private String title;
 
     @Size(max = 500, message = "Описание должно быть менее 500 символов")
+    @Schema(description = "Описание поездки", example = "Путешествие на выходные с друзьями")
     private String description;
 
     @NotNull(message = "Дата начала обязательна")
     @FutureOrPresent(message = "Дата начала должна быть в настоящем или будущем")
+    @Schema(description = "Дата начала поездки", example = "2025-07-06")
     private LocalDate startDate;
 
     @NotNull(message = "Дата конца обязательна")
     @Future(message = "Дата конца должна быть в будущем")
+    @Schema(description = "Дата окончания поездки", example = "2025-07-13")
     private LocalDate endDate;
 
     @NotNull(message = "Общий бюджет обязателен")
     @PositiveOrZero(message = "Общий бюджет должен быть положительным или нулевым")
+    @Schema(description = "Общий бюджет поездки", example = "500.0")
     private Double totalBudget;
 }
