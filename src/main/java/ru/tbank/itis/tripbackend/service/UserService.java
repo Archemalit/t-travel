9c7382fa149a3b7264cf882baf5d07f3686646b0
Archemalit <ArsenDivirov@yandex.ru>
2025-06-05 20:28:46 +0300

    fix: [TBPT2-11] убрал конфликты

--- src/main/java/ru/tbank/itis/tripbackend/service/UserService.java
+++ src/main/java/ru/tbank/itis/tripbackend/service/UserService.java
@@ -1,10 +1,18 @@
 package ru.tbank.itis.tripbackend.service;
 
-import ru.tbank.itis.tripbackend.dto.UserDto;
-import ru.tbank.itis.tripbackend.model.User;
-
-import java.util.Set;
+import jakarta.servlet.http.HttpServletRequest;
+import ru.tbank.itis.tripbackend.dto.JwtTokenPairDto;
+import ru.tbank.itis.tripbackend.dto.request.UserRegistrationRequest;
+import ru.tbank.itis.tripbackend.dto.request.UserUpdateProfileRequest;
+import ru.tbank.itis.tripbackend.dto.response.UserExistsResponse;
+import ru.tbank.itis.tripbackend.dto.response.UserProfileResponse;
 
 public interface UserService {
-    Set<User> getUserSetByUserDtoSet(Set<UserDto> members);
+    JwtTokenPairDto register(UserRegistrationRequest request);
+    UserExistsResponse doesUserExistByPhoneNumber(String phoneNumber);
+    UserProfileResponse getUserProfile(Long userId);
+    UserProfileResponse updateProfile(Long userId, UserUpdateProfileRequest request);
+//    void logout(HttpServletRequest request);
+//    AuthResponse login(UserLoginRequest request);
+//    AuthResponse refresh(RefreshTokenRequest request);
 }
